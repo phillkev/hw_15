88708dbe6cc8d49f8d2629c74dfea81d53187c37
orithal <phillkev@hotmail.com>
2019-03-12 15:28:42 -0400

    post tutor modifications

--- StarterCode/Belly_Button_Biodiversity/app.py
+++ StarterCode/Belly_Button_Biodiversity/app.py
@@ -89,17 +89,18 @@ def samples(sample):
     # only keep rows with values above 1
     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
     # Format the data to send as json
-    # sample_data.rename(columns={sample: 'sample_values'}, inplace=True)
+    sample_data.rename(columns={sample: 'sample_values'}, inplace=True)
 
-    # data = sample_data.to_json(orient='records')
+    data = sample_data.to_json(orient='records')
 
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
+    # data = {
+    #     "otu_ids": sample_data.otu_id.values.tolist(),
+    #     "sample_values": sample_data[sample].values.tolist(),
+    #     "otu_labels": sample_data.otu_label.tolist(),
+    # }
 
-    return jsonify(data)
+    # return jsonify(data)
+    return data
 
 
 if __name__ == "__main__":
